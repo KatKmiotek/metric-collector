52926c510780a5a2980f10fe0b3ae0e97cbf6096
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2024-11-27 07:54:04 +0000

    feat: add basic error handling to controller

--- src/controllers.rs
+++ src/controllers.rs
@@ -1,6 +1,8 @@
 use std::{error::Error, path::Path};
 
 use actix_web::{get, http::header::ContentType, HttpResponse};
+use serde_json::json;
+use tracing::info;
 
 use crate::metric_models::ProjectMetric;
 
@@ -10,8 +12,13 @@ pub async fn get_metrics() -> Result<HttpResponse, Box<dyn Error>> {
     let file_path = dir_path.join("metrics.txt");
     let metrics_file_content = tokio::fs::read_to_string(file_path).await?;
     let github_metrics: Vec<ProjectMetric> = serde_json::from_str(&metrics_file_content)?;
-
-    Ok(HttpResponse::Ok()
-        .content_type(ContentType::json())
-        .json(&github_metrics))
+    Ok(match github_metrics.len() {
+        0 => HttpResponse::BadRequest().json(json!({ "error": "No metrics data available" })),
+        _ => {
+            info!("Request completed with status 200");
+            HttpResponse::Ok()
+                .content_type(ContentType::json())
+                .json(&github_metrics)
+        }
+    })
 }
